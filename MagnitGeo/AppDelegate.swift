1bb90bd1eb6d699cd80a8f07a4b95206bb07dc2f
Олег Круцких <oleg.krutskih@icloud.com>
2016-03-29 22:03:24 +0300

    First commit

--- MagnitGeo/AppDelegate.swift
+++ MagnitGeo/AppDelegate.swift
@@ -13,7 +13,8 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var bgTaskID: UIBackgroundTaskIdentifier = 0
+    var timer = NSTimer()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -28,10 +29,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        // Request additional background execution time
+        
+        bgTaskID = UIApplication.sharedApplication().beginBackgroundTaskWithExpirationHandler() {
+            // Completion handler to be performed if time runs out
+            self.timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "update", userInfo: nil, repeats: true)
+            
+        }
+        
+    }
+    
+    func update() {
+        let rootViewController = self.window!.rootViewController as! ViewController
+        rootViewController.update()
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        UIApplication.sharedApplication().endBackgroundTask(bgTaskID)
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
--- MagnitGeo/ViewController.swift
+++ MagnitGeo/ViewController.swift
@@ -7,14 +7,99 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var cnt = 0
+    var timer = NSTimer()
+    var inProgress = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "update", userInfo: nil, repeats: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func findMyLocation(sender: AnyObject) {
+        if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedAlways {
+            if inProgress {
+                (sender as! UIButton).setTitle("Запустить", forState: UIControlState.Normal)
+            }
+            else {
+                (sender as! UIButton).setTitle("Остановить", forState: UIControlState.Normal)
+            }
+            inProgress = !inProgress
+        }
+        else {
+            let AlertController = UIAlertController(title: "Получение геопозиции запрещено настройками", message: "Для включения, пожалуйста перейдите в Настройки->Конфиденциальность->Службы геолокации->MagnitGeo и установите флажок напротив пункта 'Всегда'", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            //AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate)) // vibration
+            AlertController.addAction(UIAlertAction(title: "Ок", style: UIAlertActionStyle.Default,handler: nil))
+            self.presentViewController(AlertController, animated: true, completion: nil)
+        }
+    }
+    
+    func update() {
+        if inProgress {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func sendCoord(urlStr:String)
+    {
+        
+        let session = NSURLSession.sharedSession()
+        let url : NSURL = NSURL(string: urlStr)!
+        let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+            print("Coord sended.")
+            if((error) != nil) {
+                print(error!.localizedDescription)
+                return
+            }
+           
+        })
+        task.resume()
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error) ->
+            Void in
+            if (error != nil) {
+                print("Reverse geocoder failed with error" + error!.localizedDescription)
+                return
+            }
+            
+            if placemarks!.count > 0 {
+                let pm = placemarks![0] as CLPlacemark
+                print("Location updated \(self.cnt)!")
+                self.cnt += 1
+                self.displayLocationInfo(pm)
+            } else {
+                print("Problem with the data received from geocoder")
+            }
+        })
+    }
+    
+    func displayLocationInfo(placemark: CLPlacemark) {
+       
+            //stop updating location to save battery life
+            locationManager.stopUpdatingLocation()
+            sendCoord("http://www.arduino-test.h1n.ru/magnit.php/?long=\(placemark.location?.coordinate.longitude)&lat=\(placemark.location?.coordinate.latitude)")
+            print(placemark.location?.coordinate)
 
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error while updating location " + error.localizedDescription)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
